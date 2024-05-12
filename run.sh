96a9a3af42b99d5da2504bea45dfbae4a80a228f
Sergey Litvinov <slitvinov@gmail.com>
2024-05-12 18:56:39 -0400

    sort run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,24 @@
 python fish.py --fish 3
 . ./fish.sh
-exec mpiexec ./main -extentx 8 -levelMax 7 -levelStart 4 -Rtol 5 -Ctol 0.1 -bMeanConstraint 2 -bpdx 8 -bpdy 4 -bpdz 4 -nu 0.00001 -CFL 0.4 -poissonSolver iterative \
-     -tdump 0.1 -tend 1.0 -dumpOmegaX 1 -dumpOmegaY 1 dumpOmegaZ 1 -dumpVelocityX 1 -dumpVelocityY 1 -dumpVelocityZ 1 \
-     -factory-content $FACTORY
+exec mpiexec ./main \
+     -bMeanConstraint 2 \
+     -bpdx 8 \
+     -bpdy 4 \
+     -bpdz 4 \
+     -CFL 0.4 \
+     -Ctol 0.1 \
+     -dumpOmegaX 1 \
+     -dumpOmegaY 1 \
+     -dumpOmegaZ 1 \
+     -dumpVelocityX 1 \
+     -dumpVelocityY 1 \
+     -dumpVelocityZ 1 \
+     -extentx 8 \
+     -factory-content $FACTORY \
+     -levelMax 7 \
+     -levelStart 4 \
+     -nu 0.00001 \
+     -poissonSolver iterative \
+     -Rtol 5 \
+     -tdump 0.1 \
+     -tend 1.0 \
