5ce2470308ec2abc8f954bf21c8266362037e8fb
Sergey Litvinov <slitvinov@gmail.com>
2024-05-12 19:51:57 -0400

    up

--- run.sh
+++ run.sh
@@ -1,19 +1,18 @@
 exec mpiexec ./main \
      -bMeanConstraint 2 \
-     -bpdx 8 \
-     -bpdy 4 \
-     -bpdz 4 \
+     -bpdx 4 \
+     -bpdy 2 \
+     -bpdz 2 \
      -CFL 0.4 \
      -Ctol 0.1 \
      -dumpOmegaX 1 \
      -dumpOmegaY 1 \
      -dumpOmegaZ 1 \
-     -extentx 8 \
+     -extentx 4 \
      -factory-content \
-     'StefanFish L=0.2 T=1.0 xpos=3.4 ypos=1.5 zpos=1.8 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan bFixFrameOfRef=1
-      StefanFish L=0.2 T=1.0 xpos=3.9 ypos=1.7 zpos=1.5 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan
-      StefanFish L=0.2 T=1.0 xpos=3.7 ypos=2.0 zpos=1.6 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan' \
-     -levelMax 7 \
+     'StefanFish L=0.2 T=1.0 xpos=1 ypos=0.5 zpos=1 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan bFixFrameOfRef=1
+      StefanFish L=0.2 T=1.0 xpos=1 ypos=1.5 zpos=1 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan' \
+     -levelMax 6 \
      -levelStart 4 \
      -nu 0.00001 \
      -poissonSolver iterative \
