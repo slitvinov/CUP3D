e8fd5a6034c529239485ee562759ba7e745519c2
Sergey Litvinov <slitvinov@gmail.com>
2024-05-13 13:28:17 -0400

    smaller domain

--- run.sh
+++ run.sh
@@ -1,21 +1,19 @@
 OMP_NUM_THREADS=1 exec mpiexec ${exe=./main} \
      -bMeanConstraint 2 \
-     -bpdx 2 \
-     -bpdy 2 \
-     -bpdz 2 \
+     -bpdx 1 \
+     -bpdy 1 \
+     -bpdz 1 \
      -CFL 0.4 \
      -Ctol 0.1 \
-     -dumpOmegaX 1 \
-     -dumpOmegaY 1 \
-     -dumpOmegaZ 1 \
-     -extentx 2 \
+     -extentx 1 \
      -factory-content \
-     'StefanFish L=0.2 T=1.0 xpos=0.5 ypos=0.5 zpos=1 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan bFixFrameOfRef=1
-      StefanFish L=0.2 T=1.0 xpos=0.5 ypos=1.5 zpos=1 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan' \
+     'StefanFish L=0.2 T=1.0 xpos=0.4 ypos=0.5 zpos=0.25 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan bFixFrameOfRef=1
+      StefanFish L=0.2 T=1.0 xpos=0.4 ypos=0.5 zpos=0.50 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan
+      StefanFish L=0.2 T=1.0 xpos=0.4 ypos=0.5 zpos=0.75 CorrectPosition=true CorrectZ=true CorrectRoll=true heightProfile=danio widthProfile=stefan' \
      -levelMax 6 \
      -levelStart 4 \
      -nu 0.00001 \
      -poissonSolver iterative \
      -Rtol 5 \
-     -tdump 0.1 \
-     -tend 1.0 \
+     -tdump 1.0 \
+     -tend 10.0 \
