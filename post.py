3717950bcf7e3e6bfeefac56ad29cba3c60a6654
Sergey Litvinov <slitvinov@gmail.com>
2024-05-12 21:50:21 -0400

    up

--- post.py
+++ post.py
@@ -4,10 +4,7 @@ import os
 import re
 import xml.etree.ElementTree
 
-# f = h5py.File("data/chi_000000001.h5")
-# print(f)
-
-path = "data/chi_000000001.xmf"
+path = "data/chi_000000940.xmf"
 x = xml.etree.ElementTree.parse(path)
 root = x.getroot()
 hdf_data = root.find('.//DataItem[@Format="HDF"]')
@@ -18,18 +15,22 @@ hdf_data = re.sub("^.*:", "", hdf_path)
 hdf_path = re.sub(":[^:]*$", "", hdf_path)
 dirname = os.path.dirname(path)
 hdf_path = os.path.join(dirname, hdf_path)
-
-with h5py.File(hdf_path) as f:
+with h5py.File(hdf_path) as f, open("a.txt", "w") as fo:
     offset = 0
     for grid in root.findall('.//Grid[@GridType="Uniform"]'):
         topology = grid.find('./Topology')
         geometry = grid.find('./Geometry[@GeometryType="ORIGIN_DXDYDZ"]')
         dorg, dspa = geometry.findall("./DataItem")
         ddim = topology.get("Dimensions")
-        org = [float(e) for e in dorg.text.split()]
-        spa = [float(e) for e in dspa.text.split()]
+        ox, oy, oz = [float(e) for e in dorg.text.split()]
+        sx, sy, sz = [float(e) for e in dspa.text.split()]
         nx, ny, nz = [int(e) for e in ddim.split()]
         size = (nx - 1) * (ny - 1) * (nz - 1)
         data = f[hdf_data][offset:offset + size]
+        data = data.reshape((nx - 1, ny - 1, nz - 1))
+        ix, iy, iz = np.where(data > 0)
+        x = (ix + 1 / 2) * sx + ox
+        y = (iy + 1 / 2) * sy + oy
+        z = (iz + 1 / 2) * sz + oz
+        np.savetxt(fo, np.column_stack((x, y, z)))
         offset += size
-        print(data)
